26b9b742b2a41ce44a28f93c5356e8e9e1285bf0
mktoho <mk@mktoho.org>
2025-04-05 04:50:21 +0900

    OGまわり

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,37 @@ import { auth } from '@/auth'
 import SignIn from '@/component/sign-in'
 import { SignOut } from '@/component/sign-out'
 
+const title = 'ZEN大Slackのチャンネル一覧'
+const description = 'ZEN大学のSlackチャンネルを表示するサイトです。'
+const url = 'https://zen-slack-channels.mktoho.dev'
+
+export const metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: 'ZenChannels',
+    images: [
+      {
+        url: `${url}/zen-channels-ogp.png`,
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    images: [`${url}/zen-channels-ogp.png`],
+    creator: '@mktoho12',
+  },
+}
+
 export default async function Home() {
   const session = await auth()
 
